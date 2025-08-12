39a5484f9d3f1f0dcf4b4f86ed2dabae8ade48a8
Mannati Meshram <mannatimeshram@gmail.com>
2025-08-12 23:40:55 +0530

    Add files via upload
    a. calculator.py: a basic calculator code to solve input programs 
    b. true_false.py : a basic code about binary (0,1) number use in code language .

---
+++ calculator.py.py
@@ -0,0 +1,16 @@
+# a simple calculator 
+a= int(input(" Enter value of a :"))
+b= int(input(" Enter value of b :"))
+
+sum = a+b
+print(sum)
+
+subtract = a-b
+print(subtract) 
+
+product = a*b
+print(product)
+
+division= a/b
+print(division )
+
