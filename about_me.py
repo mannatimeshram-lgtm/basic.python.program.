26dd289f9abf8fd8bafffc1355b4bd10a7b4b20a
Mannati Meshram <mannatimeshram@gmail.com>
2025-08-16 16:42:12 +0530

    Add files via upload
    started with basic python .

---
+++ about_me.py
@@ -0,0 +1,9 @@
+name= "Mannati"
+age= 19
+education= "First year "
+fvt_language= "Python "
+
+print("My name is:",name )
+print("My age is :", age)
+print ("Studying in:", education)
+print("My first progarmimg language is ", fvt_language)
